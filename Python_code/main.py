b260320c75d1a91d688f2e3583ae8207ecabced9
Carl J. Bialorucki <cbialo2@outlook.com>
2021-10-04 16:08:04 -0600

    Added Karla's Greeting.

--- Python_code/main.py
+++ Python_code/main.py
@@ -1,10 +1,11 @@
-import Jake, bialorucki, Sommer
+import Jake, bialorucki, Sommer, sommerfeldt
 
 def main():
     print("Welcome to our collaborative program.")
     bialorucki.greeting()
     Jake.greeting()
     Sommer.greeting()
+    sommerfeldt.karla()
 
 if __name__ == '__main__':
     main()
---
+++ Python_code/sommerfeldt.py
@@ -0,0 +1,6 @@
+
+def karla():
+
+    print("Greetings form Canada!")
+    
+    
